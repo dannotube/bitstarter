b543fbb44889077a62c1755d114b2edcf909c9a7
dannotube <danyo20@gmail.com>
2013-07-13 07:26:29 +0000

    Add ability to input URL

--- grader.js
+++ grader.js
@@ -22,10 +22,13 @@ References:
 */
 
 var fs = require('fs');
+var fs2 = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "default_url";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,8 +68,21 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to html file', URL_DEFAULT)
     .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    
+    var TEMP_FILE;
+    if (program.url != URL_DEFAULT)
+    {
+	TEMP_FILE="temp.html";
+       	rest.get(program.url).on('complete', function(data) {
+	    fs2.writeFileSync(TEMP_FILE, data);
+	});
+    }
+
+    var htmlfile = TEMP_FILE || program.file;
+
+    var checkJson = checkHtmlFile(htmlfile, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
